6056f7062fccb88178797c7c148165a2ed513dbc
betty2310 <huynh.dh2310@gmail.com>
2023-01-23 19:33:00 +0700

    chore: add javafx swing dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module hust.soict.hedspi.javafx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.swing;
     requires de.jensd.fx.glyphs.fontawesome;
+    requires java.desktop;
 
 
     opens painter to javafx.fxml;
