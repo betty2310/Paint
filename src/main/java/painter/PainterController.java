def3849e182cf2ebe2935b6f0c5ad97d8ece545d
betty2310 <huynh.dh2310@gmail.com>
2023-01-24 17:02:33 +0700

    feat(tool): init erase button

--- src/main/java/painter/PainterController.java
+++ src/main/java/painter/PainterController.java
@@ -7,6 +7,7 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,6 +15,7 @@ import java.util.ResourceBundle;
 
 public class PainterController implements Initializable {
     public ToggleButton btPen;
+    public ToggleButton btErase;
     private GraphicsContext gc;
     public Button btSave;
     public Button btOpen;
@@ -23,11 +25,41 @@ public class PainterController implements Initializable {
     public Button btRedo;
     public Canvas canvas;
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        gc = canvas.getGraphicsContext2D();
+        gc.setLineWidth(1);
+    }
+
+    public void clearCanvas(ActionEvent actionEvent) {
+    }
+
+    public void undoCanvas(ActionEvent actionEvent) {
+    }
+    public void redoCanvas(ActionEvent actionEvent) {
+    }
+
+    public void toggleErase(ActionEvent actionEvent) {
+        if(btErase.isSelected()) {
+            btPen.setSelected(false);
+        }
+    }
+
     public void togglePen(ActionEvent actionEvent) {
+        if(btPen.isSelected()) {
+            btErase.setSelected(false);
+        }
     }
     public void setOnMousePressed(MouseEvent mouseEvent) {
         if (btPen.isSelected()) {
             gc.setStroke(javafx.scene.paint.Color.BLACK);
+            gc.setLineWidth(1);
+            gc.beginPath();
+            gc.lineTo(mouseEvent.getX(), mouseEvent.getY());
+        }
+        if(btErase.isSelected()) {
+            gc.setStroke(Color.WHITE);
+            gc.setLineWidth(10);
             gc.beginPath();
             gc.lineTo(mouseEvent.getX(), mouseEvent.getY());
         }
@@ -38,20 +70,22 @@ public class PainterController implements Initializable {
             gc.lineTo(mouseEvent.getX(), mouseEvent.getY());
             gc.stroke();
         }
+        if (btErase.isSelected()) {
+            gc.lineTo(mouseEvent.getX(), mouseEvent.getY());
+            gc.stroke();
+        }
     }
 
     public void setOnMouseReleased(MouseEvent mouseEvent) {
-            if (btPen.isSelected()) {
-                gc.lineTo(mouseEvent.getX(), mouseEvent.getY());
-                gc.stroke();
-                gc.closePath();
-            }
+        if (btPen.isSelected()) {
+            gc.lineTo(mouseEvent.getX(), mouseEvent.getY());
+            gc.stroke();
+            gc.closePath();
+        }
+        if (btErase.isSelected()) {
+            gc.lineTo(mouseEvent.getX(), mouseEvent.getY());
+            gc.stroke();
+            gc.closePath();
+        }
     }
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        gc = canvas.getGraphicsContext2D();
-        gc.setLineWidth(1);
-    }
-
 }
