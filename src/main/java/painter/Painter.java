5ba0e8134f25aae989dced4c966cbb2a3844686c
betty2310 <huynh.dh2310@gmail.com>
2023-01-28 12:28:02 +0700

    feat(icons): init app and dock

--- src/main/java/painter/Painter.java
+++ src/main/java/painter/Painter.java
@@ -3,8 +3,10 @@ package painter;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class Painter  extends Application {
@@ -12,6 +14,18 @@ public class Painter  extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Painter.class.getResource("Painter.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/images/app.png")));
+
+        //Set icon on the taskbar/dock
+        if (Taskbar.isTaskbarSupported()) {
+            var taskbar = Taskbar.getTaskbar();
+            if (taskbar.isSupported(Taskbar.Feature.ICON_IMAGE)) {
+                final Toolkit defaultToolkit = Toolkit.getDefaultToolkit();
+                var dockIcon = defaultToolkit.getImage(getClass().getResource("/images/app.png"));
+                taskbar.setIconImage(dockIcon);
+            }
+
+        }
         stage.setTitle("Painter");
         stage.setScene(scene);
         stage.show();
